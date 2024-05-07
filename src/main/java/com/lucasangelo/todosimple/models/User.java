10ddcd7573e0c450dcd7f4d99b24f98248909dfd
ghscesario <gabriel.hscesario@gmail.com>
2024-05-07 11:28:38 -0300

    Model User e Task

--- src/main/java/com/lucasangelo/todosimple/models/User.java
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -1,10 +1,14 @@
 package com.lucasangelo.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -41,7 +45,8 @@ public class User {
     @Size(groups = {CreateUser.class,UpdateUser.class}, min = 8,max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -70,6 +75,22 @@ public class User {
         this.username = username;
     }
 
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
     
 
 }
