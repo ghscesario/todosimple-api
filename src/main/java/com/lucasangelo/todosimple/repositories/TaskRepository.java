05ccc86657103d04a5689a45b70e2d651b0404f1
ghscesario <gabriel.hscesario@gmail.com>
2024-05-13 10:11:49 -0300

    Task Controller

--- src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -1,5 +1,7 @@
 package com.lucasangelo.todosimple.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.lucasangelo.todosimple.models.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
     
+    List<Task> findByUser_Id(Long id);
+
 }
